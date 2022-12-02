9abb86ca0ba9b7696deae8046da544c1debf9fb4
Brendan Ralston <brendan@ralston.io>
2022-12-01 21:35:45 -0500

    Add Day1/Part1 initial solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var currentMax = Int.MIN_VALUE
+        var runningSum = 0
+
+        for (i in input) {
+            // Check that the current value is a number
+            if (i.toIntOrNull() != null) {
+                runningSum += i.toInt()
+            } else {
+                // Space is delimiter
+                currentMax = currentMax.coerceAtLeast(runningSum)
+                runningSum = 0
+            }
+        }
+
+        return currentMax
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +22,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01TestInput")
+    check(part1(testInput) == 24_000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    // Solve Part 1
+    val part1Input = readInput("Day01Input")
+    println("Part 1 solution: ${part1(part1Input)}")
 }
